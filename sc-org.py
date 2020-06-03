47f59bd029926885b35b5287028c36522ef32e78
Spencer Larsen <larsen.spencer@gmail.com>
2020-06-03 07:40:10 -0700

    BOT adds date and time stamps in separate messages, along with default reaction

--- sc-org.py
+++ sc-org.py
@@ -1,9 +1,18 @@
 import os, re
 import discord
 from dotenv import load_dotenv
+from datetime import date
+
+
+# discord bot image: https://www.vectorstock.com/royalty-free-vector/pi-symbol-with-infinity-sign-abstract-icon-vector-8286697
+# api ref: https://discordpy.readthedocs.io/en/latest/api.html#
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_LIST_RAW = os.getenv('CHANNELS')
+
+ch_list = CHANNEL_LIST_RAW.split(',')
+print(ch_list,flush=True)
 
 client = discord.Client()
 
@@ -13,30 +22,32 @@ async def on_ready():
     for guild in client.guilds:
         # print(guild.name,flush=True)
     
-        print(f'{client.user} has connected to Discord!\n {guild.name}(id: {guild.id})\n',flush=True)
+        # print(f'{client.id} has connected to Discord!\n {guild.name}(id: {guild.id})\n',flush=True)
+        print(f'Connected to Discord!\n {guild.name}(id: {guild.id})\n',flush=True)
+        for channel in guild.channels:
+            # list channels in guild
+            # print(f'- {channel.name}(id: {channel.id})\n',flush=True)
 
+            if str(channel.id) in ch_list:
 
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-    
-#     if re.match(r'^\!+.',message.content):
-        
-#         # list instructions for std user
+                today = date.today()
 
-#         # list followers
+                targetId = 717743773781852232
+                
+                async for message in channel.history(limit=100):
+                    if message.id == targetId:
+                        date_str = "Today\'s Date: " + str(today.strftime("%B %d, %Y"))
+                        await message.edit(content=str(date_str))
 
-#         # add follower (check if in list already, otherwise add, restart twitter stream with new followers)
-#         # require approval from admin in DM?
+                async for message in channel.history(limit=20):
+                #     print(f'{message.id}: {message.content}')
+                    if message.author == client.user and message.id != targetId:
+                        await message.clear_reactions()
+                        await message.add_reaction('<:3783_salute1:652257869927940107>')
 
-#         # remove follower (verify if in list of approved users who can remove first)
-
-
-        
-#         response = "Command entered"
-#         await message.channel.send(response)
-
-
+                
+                #         # await message.delete()
+    # close the client for the cronjob to function properly
+    await client.close()
 
 client.run(TOKEN)
